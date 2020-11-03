00cdef8e4642b47bccad7cb73e2eff36b6d7201a
Darja Fedosova <darjafedosova@MacBook-Pro-Darja.local>
2020-11-04 01:05:58 +0200

    init  commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+//
+// вывести на экран словосочетание Hello JavaGURU World
+//
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello JavaGURU World");
+    }
+}
